255deb80adc61edc7ea407847f63ca1bfea30ced
Khin Thiri Kyaw <khinthirikyaw.b@gmail.com>
2024-01-24 09:32:44 +0630

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/styles/custom.css">
+    <title>Pogue Bag Store</title>
+</head>
+
+<body>
+    <h1 class="text-primary">Hello Bootstrap</h1>
+    <button class="btn btn-primary">Click Me</button>
+</body>
+
+</html>
